b089dac54cb65a41e9b382887e6ad0a495d79937
Samuel McAravey <mcaravey@rixian.net>
2015-06-18 11:10:08 -0600

    Added initial code and markdown for IdentityServer3.

---
+++ Markdown/IdentityServer3.md
@@ -0,0 +1,8 @@
+# IdentityServer3
+
+Alright, we are back and looking at IdentityServer3. I assume you already know what it is and how it basically works. Also before we begin please read through this article in their documentation, as we will start with this:
+http://identityserver.github.io/Documentation/docs/overview/mvcGettingStarted.html
+
+And... we're back. So we are going to take our MembershipReboot code from last time and add a WebApi project to become our IdentityServer. Let's add it and reference the following NuGet packages:
+
+![alt text](Images/ProjectSetup-LocalDbEntry.PNG "User Account Entry in LocalDB")
---
+++ Source/MRTutorial-IdentityServer3/MRTutorial.Membership.Tests/MembershipTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MRTutorial.Membership.Tests
+{
+    [TestClass]
+    public class MembershipTests
+    {
+        private const string ConnectionString =
+            @"Data Source=(localdb)\ProjectsV12;Initial Catalog=MembershipReboot;Integrated Security=True";
+
+        [TestMethod]
+        public void CreateDeleteAccount_Valid_Success()
+        {
+            Membership membership = new Membership(ConnectionString);
+            try
+            {
+                membership.CreateAccount("foo", "bar", "foo@bar.com");
+                Assert.IsNotNull(membership.Account);
+            }
+            finally
+            {
+                membership.DeleteAccount();
+                Assert.IsNull(membership.Account);
+            }
+        }
+
+        [TestMethod]
+        public void Authentication_Valid_Success()
+        {
+            Membership membership = new Membership(ConnectionString);
+            try
+            {
+                membership.CreateAccount("foo", "bar", "foo@bar.com");
+                Assert.IsNotNull(membership.Account);
+                var authenticated = membership.Authenticate("foo", "bar");
+                Assert.IsTrue(authenticated);
+            }
+            finally
+            {
+                membership.DeleteAccount();
+                Assert.IsNull(membership.Account);
+            }
+        }
+
+        [TestMethod]
+        public void Authentication_NoAccount_Success()
+        {
+            Membership membership = new Membership(ConnectionString);
+            try
+            {
+                membership.CreateAccount("foo", "bar", "foo@bar.com");
+                Assert.IsNotNull(membership.Account);
+                membership = new Membership(ConnectionString);
+                Assert.IsNull(membership.Account);
+                var authenticated = membership.Authenticate("foo", "bar");
+                Assert.IsTrue(authenticated);
+            }
+            finally
+            {
+                membership.DeleteAccount();
+                Assert.IsNull(membership.Account);
+            }
+        }
+    }
+}
